e8b20dc9e6368b68bb336c45206b4d8fad4ca989
dvasyliev <dvasyliev.dev@gmail.com>
2017-07-25 07:24:53 +0300

    Added styles for app component (skeleton layout)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule} from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { PlayerListComponent } from './Components/players/player-list/player-list.component';
+import {AppComponent} from './app.component';
+import {PlayerSearchComponent} from './Components/players/player-search/player-search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PlayerListComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PlayerSearchComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
